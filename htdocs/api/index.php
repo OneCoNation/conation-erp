825353f434b62de827571e0fe06319661a3de41e
the name`s rynn <andiargonnawork@gmail.com>
2026-02-15 14:48:29 +0700

    debugging  documents

--- htdocs/api/index.php
+++ htdocs/api/index.php
@@ -56,7 +56,7 @@ if (!defined("NODEFAULTVALUES")) {
 
 // Force entity if a value is provided into HTTP header. Otherwise, will use the entity of user of token used.
 if (!empty($_SERVER['HTTP_DOLAPIENTITY'])) {
-	define("DOLENTITY", (int) $_SERVER['HTTP_DOLAPIENTITY']);
+	define("DOLENTITY", (int)$_SERVER['HTTP_DOLAPIENTITY']);
 }
 
 // Response for preflight requests (used by browser when into a CORS context)
@@ -91,22 +91,22 @@ if (!$res) {
 	die("Include of main fails");
 }
 
-require_once DOL_DOCUMENT_ROOT.'/includes/restler/framework/Luracast/Restler/AutoLoader.php';
+require_once DOL_DOCUMENT_ROOT . '/includes/restler/framework/Luracast/Restler/AutoLoader.php';
 
 call_user_func(
-	/**
-	 * @return Luracast\Restler\AutoLoader
-	 */
-	static function () {
-		$loader = Luracast\Restler\AutoLoader::instance();
-		spl_autoload_register($loader);
-		return $loader;
-	}
+/**
+ * @return Luracast\Restler\AutoLoader
+ */
+static function () {
+	$loader = Luracast\Restler\AutoLoader::instance();
+	spl_autoload_register($loader);
+	return $loader;
+}
 );
 
-require_once DOL_DOCUMENT_ROOT.'/api/class/api.class.php';
-require_once DOL_DOCUMENT_ROOT.'/api/class/api_access.class.php';
-require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
+require_once DOL_DOCUMENT_ROOT . '/api/class/api.class.php';
+require_once DOL_DOCUMENT_ROOT . '/api/class/api_access.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/lib/functions2.lib.php';
 /**
  * @var Conf $conf
  * @var DoliDB $db
@@ -117,8 +117,8 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
 
 
 $url = $_SERVER['PHP_SELF'];
-if (preg_match('/api\/index\.php$/', $url)) {	// sometimes $_SERVER['PHP_SELF'] is 'api\/index\.php' instead of 'api\/index\.php/explorer.php' or 'api\/index\.php/method'
-	$url = $_SERVER['PHP_SELF'].(empty($_SERVER['PATH_INFO']) ? $_SERVER['ORIG_PATH_INFO'] : $_SERVER['PATH_INFO']);
+if (preg_match('/api\/index\.php$/', $url)) { // sometimes $_SERVER['PHP_SELF'] is 'api\/index\.php' instead of 'api\/index\.php/explorer.php' or 'api\/index\.php/method'
+	$url = $_SERVER['PHP_SELF'] . (empty($_SERVER['PATH_INFO']) ? $_SERVER['ORIG_PATH_INFO'] : $_SERVER['PATH_INFO']);
 }
 // Fix for some NGINX setups (this should not be required even with NGINX, however setup of NGINX are often mysterious and this may help is such cases)
 if (getDolGlobalString('MAIN_NGINX_FIX')) {
@@ -129,7 +129,7 @@ if (getDolGlobalString('MAIN_NGINX_FIX')) {
 if (!isModEnabled('api')) {
 	$langs->load("admin");
 	dol_syslog("Call of Dolibarr API interfaces with module API REST are disabled");
-	print $langs->trans("WarningModuleNotActive", 'Api').'.<br><br>';
+	print $langs->trans("WarningModuleNotActive", 'Api') . '.<br><br>';
 	print $langs->trans("ToActivateModule");
 	//session_destroy();
 	exit(0);
@@ -139,7 +139,7 @@ if (!isModEnabled('api')) {
 if (preg_match('/api\/index\.php\/explorer/', $url) && getDolGlobalString('API_EXPLORER_DISABLED')) {
 	$langs->load("admin");
 	dol_syslog("Call Dolibarr API interfaces with module API REST disabled");
-	print $langs->trans("WarningAPIExplorerDisabled").'.<br><br>';
+	print $langs->trans("WarningAPIExplorerDisabled") . '.<br><br>';
 	//session_destroy();
 	exit(0);
 }
@@ -174,7 +174,7 @@ $refreshcache = (getDolGlobalString('API_PRODUCTION_DO_NOT_ALWAYS_REFRESH_CACHE'
 if (!empty($reg[1]) && $reg[1] == 'explorer' && ($reg[2] == '/swagger.json' || $reg[2] == '/swagger.json/root' || $reg[2] == '/resources.json' || $reg[2] == '/resources.json/root')) {
 	$refreshcache = true;
 	if (!is_writable($conf->api->dir_temp)) {
-		dol_syslog("ErrorFailedToWriteInApiTempDirectory ".$conf->api->dir_temp, LOG_ERR);
+		dol_syslog("ErrorFailedToWriteInApiTempDirectory " . $conf->api->dir_temp, LOG_ERR);
 		print 'Erreur temp dir api/temp not writable';
 		header('HTTP/1.1 500 temp dir api/temp not writable');
 		exit(0);
@@ -196,9 +196,9 @@ if (getDolGlobalString('MAIN_API_DEBUG')) {
 		//	'version' => $api->r->getRequestedApiVersion(),
 		//	'data'    => $api->r->getRequestData(),
 		//dol_syslog("Debug API input ".var_export($r, true), LOG_DEBUG, 0, '_api');
-		dol_syslog("Debug API url ".var_export($r->url, true), LOG_DEBUG, 0, '_api');
-		dol_syslog("Debug API input ".var_export($r->getRequestData(), true), LOG_DEBUG, 0, '_api');
-		//}
+		dol_syslog("Debug API url " . var_export($r->url, true), LOG_DEBUG, 0, '_api');
+		dol_syslog("Debug API input " . var_export($r->getRequestData(), true), LOG_DEBUG, 0, '_api');
+	//}
 	});
 }
 
@@ -219,9 +219,9 @@ if (getDolGlobalString('API_RESTRICT_ON_IP')) {
 	$allowedip = explode(' ', getDolGlobalString('API_RESTRICT_ON_IP'));
 	$ipremote = getUserRemoteIP();
 	if (!in_array($ipremote, $allowedip)) {
-		dol_syslog('Remote ip is '.$ipremote.', not into list ' . getDolGlobalString('API_RESTRICT_ON_IP'));
-		print 'APIs are not allowed from the IP '.$ipremote;
-		header('HTTP/1.1 503 API not allowed from your IP '.$ipremote);
+		dol_syslog('Remote ip is ' . $ipremote . ', not into list ' . getDolGlobalString('API_RESTRICT_ON_IP'));
+		print 'APIs are not allowed from the IP ' . $ipremote;
+		header('HTTP/1.1 503 API not allowed from your IP ' . $ipremote);
 		//session_destroy();
 		exit(0);
 	}
@@ -237,27 +237,30 @@ if (!empty($reg[1]) && $reg[1] == 'explorer' && ($reg[2] == '/swagger.json' || $
 	$modulesdir = dolGetModulesDirs();
 	foreach ($modulesdir as $dir) {
 		// Search available module
-		dol_syslog("Scan directory ".$dir." for module descriptor files, then search for API files");
+		dol_syslog("Scan directory " . $dir . " for module descriptor files, then search for API files");
 
 		$handle = @opendir(dol_osencode($dir));
 		if (is_resource($handle)) {
 			while (($file = readdir($handle)) !== false) {
 				$regmod = array();
-				if (is_readable($dir.$file) && preg_match("/^mod(.*)\.class\.php$/i", $file, $regmod)) {
+				if (is_readable($dir . $file) && preg_match("/^mod(.*)\.class\.php$/i", $file, $regmod)) {
 					$module = strtolower($regmod[1]);
 					$moduledirforclass = getModuleDirForApiClass($module);
 					$modulenameforenabled = $module;
 					if ($module == 'propale') {
 						$modulenameforenabled = 'propal';
-					} elseif ($module == 'supplierproposal') {
+					}
+					elseif ($module == 'supplierproposal') {
 						$modulenameforenabled = 'supplier_proposal';
-					} elseif ($module == 'ficheinter') {
+					}
+					elseif ($module == 'ficheinter') {
 						$modulenameforenabled = 'intervention';
-					} elseif ($module == 'product' && !isModEnabled('product') && isModEnabled('service')) {
+					}
+					elseif ($module == 'product' && !isModEnabled('product') && isModEnabled('service')) {
 						$modulenameforenabled = 'service';
 					}
 
-					dol_syslog("Found module file ".$file." - module=".$module." - modulenameforenabled=".$modulenameforenabled." - moduledirforclass=".$moduledirforclass);
+					dol_syslog("Found module file " . $file . " - module=" . $module . " - modulenameforenabled=" . $modulenameforenabled . " - moduledirforclass=" . $moduledirforclass);
 
 					// Defined if module is enabled
 					$enabled = true;
@@ -269,7 +272,7 @@ if (!empty($reg[1]) && $reg[1] == 'explorer' && ($reg[2] == '/swagger.json' || $
 						// If exists, load the API class for enable module
 						// Search files named api_<object>.class.php into /htdocs/<module>/class directory
 						// @todo : use getElementProperties() function ?
-						$dir_part = dol_buildpath('/'.$moduledirforclass.'/class/');
+						$dir_part = dol_buildpath('/' . $moduledirforclass . '/class/');
 
 						$handle_part = @opendir(dol_osencode($dir_part));
 						if (is_resource($handle_part)) {
@@ -286,18 +289,20 @@ if (!empty($reg[1]) && $reg[1] == 'explorer' && ($reg[2] == '/swagger.json' || $
 								//dol_syslog("We scan to search api file with into ".$dir_part.$file_searched);
 
 								$regapi = array();
-								if (is_readable($dir_part.$file_searched) && preg_match("/^api_(.*)\.class\.php$/i", $file_searched, $regapi)) {
+								if (is_readable($dir_part . $file_searched) && preg_match("/^api_(.*)\.class\.php$/i", $file_searched, $regapi)) {
 									$classname = ucwords($regapi[1]);
 									$classname = str_replace('_', '', $classname);
-									require_once $dir_part.$file_searched;
-									if (class_exists($classname.'Api')) {
+									require_once $dir_part . $file_searched;
+									if (class_exists($classname . 'Api')) {
 										//dol_syslog("Found API by index.php: classname=".$classname."Api for module ".$dir." into ".$dir_part.$file_searched);
-										$listofapis[strtolower($classname.'Api')] = $classname.'Api';
-									} elseif (class_exists($classname)) {
+										$listofapis[strtolower($classname . 'Api')] = $classname . 'Api';
+									}
+									elseif (class_exists($classname)) {
 										//dol_syslog("Found API by index.php: classname=".$classname." for module ".$dir." into ".$dir_part.$file_searched);
 										$listofapis[strtolower($classname)] = $classname;
-									} else {
-										dol_syslog("We found an api_xxx file (".$file_searched.") but class ".$classname." does not exists after loading file", LOG_WARNING);
+									}
+									else {
+										dol_syslog("We found an api_xxx file (" . $file_searched . ") but class " . $classname . " does not exists after loading file", LOG_WARNING);
 									}
 								}
 							}
@@ -314,14 +319,14 @@ if (!empty($reg[1]) && $reg[1] == 'explorer' && ($reg[2] == '/swagger.json' || $
 	foreach ($listofapis as $apiname => $classname) {
 		$api->r->addAPIClass($classname, $apiname);
 	}
-	//var_dump($api->r);
+//var_dump($api->r);
 }
 
 // Call one APIs or one definition of an API
 $regbis = array();
 if (!empty($reg[1]) && ($reg[1] != 'explorer' || ($reg[2] != '/swagger.json' && $reg[2] != '/resources.json' && preg_match('/^\/(swagger|resources)\.json\/(.+)$/', $reg[2], $regbis) && $regbis[2] != 'root'))) {
 	$moduleobject = $reg[1];
-	if ($moduleobject == 'explorer') {  // If we call page to explore details of a service
+	if ($moduleobject == 'explorer') { // If we call page to explore details of a service
 		$moduleobject = $regbis[2];
 	}
 
@@ -329,7 +334,7 @@ if (!empty($reg[1]) && ($reg[1] != 'explorer' || ($reg[2] != '/swagger.json' && 
 	$moduledirforclass = getModuleDirForApiClass($moduleobject);
 
 	// Load a dedicated API file
-	dol_syslog("Load a dedicated API file moduleobject=".$moduleobject." moduledirforclass=".$moduledirforclass);
+	dol_syslog("Load a dedicated API file moduleobject=" . $moduleobject . " moduledirforclass=" . $moduledirforclass);
 
 	$tmpmodule = $moduleobject;
 	if ($tmpmodule != 'api') {
@@ -357,7 +362,7 @@ if (!empty($reg[1]) && ($reg[1] != 'explorer' || ($reg[2] != '/swagger.json' && 
 		$moduledirforclass = 'eventorganization';
 	}
 
-	$dir_part_file = dol_buildpath('/'.$moduledirforclass.'/class/api_'.$classfile.'.class.php', 0, 2);
+	$dir_part_file = dol_buildpath('/' . $moduledirforclass . '/class/api_' . $classfile . '.class.php', 0, 2);
 
 	$classname = ucwords($moduleobject);
 
@@ -369,15 +374,15 @@ if (!empty($reg[1]) && ($reg[1] != 'explorer' || ($reg[2] != '/swagger.json' && 
 
 		foreach ($listofendpoints as $endpointrule) {
 			$tmparray = explode(':', $endpointrule);
-			if (($classfile == $tmparray[0] || $classfile.'api' == $tmparray[0]) && $tmparray[1] == 1) {
+			if (($classfile == $tmparray[0] || $classfile . 'api' == $tmparray[0]) && $tmparray[1] == 1) {
 				$endpointisallowed = true;
 				break;
 			}
 		}
 
-		if (! $endpointisallowed) {
-			dol_syslog('The API with endpoint /'.$classfile.' is forbidden by config API_ENDPOINT_RULES', LOG_WARNING);
-			print 'The API with endpoint /'.$classfile.' is forbidden by config API_ENDPOINT_RULES';
+		if (!$endpointisallowed) {
+			dol_syslog('The API with endpoint /' . $classfile . ' is forbidden by config API_ENDPOINT_RULES', LOG_WARNING);
+			print 'The API with endpoint /' . $classfile . ' is forbidden by config API_ENDPOINT_RULES';
 			header('HTTP/1.1 501 API is forbidden by API_ENDPOINT_RULES');
 			//session_destroy();
 			exit(0);
@@ -390,17 +395,17 @@ if (!empty($reg[1]) && ($reg[1] != 'explorer' || ($reg[2] != '/swagger.json' && 
 	// Note that $action and $object may be modified by some hooks
 	$reshook = $hookmanager->executeHooks('beforeApiCall', $parameters, $object, $action);
 	if ($reshook < 0) {
-		dol_syslog('beforeapicall Failed to call hook '.$hookmanager->error, LOG_ERR);
+		dol_syslog('beforeapicall Failed to call hook ' . $hookmanager->error, LOG_ERR);
 	}
 
-	dol_syslog('Search api file /'.$moduledirforclass.'/class/api_'.$classfile.'.class.php => dir_part_file='.$dir_part_file.', classname='.$classname);
+	dol_syslog('Search api file /' . $moduledirforclass . '/class/api_' . $classfile . '.class.php => dir_part_file=' . $dir_part_file . ', classname=' . $classname);
 
 	$res = false;
 	if ($dir_part_file) {
 		$res = include_once $dir_part_file;
 	}
 	if (!$res) {
-		dol_syslog('Failed to make include_once '.$dir_part_file, LOG_WARNING);
+		dol_syslog('Failed to make include_once ' . $dir_part_file, LOG_WARNING);
 		print 'API not found (failed to include API file)';
 		header('HTTP/1.1 501 API not found (failed to include API file)');
 		//session_destroy();
@@ -448,13 +453,16 @@ if (Luracast\Restler\Defaults::$returnResponse) {
 	if (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'br') !== false && function_exists('brotli_compress') && defined('BROTLI_TEXT')) {
 		header('Content-Encoding: br');
 		$result = brotli_compress($responsedata, 11, constant('BROTLI_TEXT'));
-	} elseif (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'bz') !== false && function_exists('bzcompress')) {
+	}
+	elseif (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'bz') !== false && function_exists('bzcompress')) {
 		header('Content-Encoding: bz');
 		$result = bzcompress($responsedata, 9);
-	} elseif (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false && function_exists('gzencode')) {
+	}
+	elseif (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false && function_exists('gzencode')) {
 		header('Content-Encoding: gzip');
 		$result = gzencode($responsedata, 9);
-	} else {
+	}
+	else {
 		header('Content-Encoding: text/html');
 		print "No compression method found. Try to disable compression by adding API_DISABLE_COMPRESSION=1";
 		exit(0);
@@ -470,48 +478,51 @@ if (getDolGlobalInt("API_ENABLE_COUNT_CALLS") && $api->r->responseCode == 200) {
 	$userid = DolibarrApiAccess::$user->id;
 
 	$sql = "SELECT up.value";
-	$sql .= " FROM ".MAIN_DB_PREFIX."user_param as up";
+	$sql .= " FROM " . MAIN_DB_PREFIX . "user_param as up";
 	$sql .= " WHERE up.param = 'API_COUNT_CALL'";
-	$sql .= " AND up.fk_user = ".((int) $userid);
-	$sql .= " AND up.entity = ".((int) $conf->entity);
+	$sql .= " AND up.fk_user = " . ((int)$userid);
+	$sql .= " AND up.entity = " . ((int)$conf->entity);
 
 	$result = $db->query($sql);
 	if ($result) {
 		$updateapi = false;
 		$nbrows = $db->num_rows($result);
 		if ($nbrows == 0) {
-			$sql2 = "INSERT INTO ".MAIN_DB_PREFIX."user_param";
+			$sql2 = "INSERT INTO " . MAIN_DB_PREFIX . "user_param";
 			$sql2 .= " (fk_user, entity, param, value)";
-			$sql2 .= " VALUES (".((int) $userid).", ".((int) $conf->entity).", 'API_COUNT_CALL', 1)";
-		} else {
+			$sql2 .= " VALUES (" . ((int)$userid) . ", " . ((int)$conf->entity) . ", 'API_COUNT_CALL', 1)";
+		}
+		else {
 			$updateapi = true;
-			$sql2 = "UPDATE ".MAIN_DB_PREFIX."user_param as up";
+			$sql2 = "UPDATE " . MAIN_DB_PREFIX . "user_param as up";
 			$sql2 .= " SET up.value = up.value + 1";
 			$sql2 .= " WHERE up.param = 'API_COUNT_CALL'";
-			$sql2 .= " AND up.fk_user = ".((int) $userid);
-			$sql2 .= " AND up.entity = ".((int) $conf->entity);
+			$sql2 .= " AND up.fk_user = " . ((int)$userid);
+			$sql2 .= " AND up.entity = " . ((int)$conf->entity);
 		}
 
 		$result2 = $db->query($sql2);
 		if (!$result2) {
 			$modeapicall = $updateapi ? 'updating' : 'inserting';
-			dol_syslog('Error while '.$modeapicall. ' API_COUNT_CALL for user '.$userid, LOG_ERR);
+			dol_syslog('Error while ' . $modeapicall . ' API_COUNT_CALL for user ' . $userid, LOG_ERR);
 			$error++;
 		}
-	} else {
-		dol_syslog('Error on select API_COUNT_CALL for user '.$userid, LOG_ERR);
+	}
+	else {
+		dol_syslog('Error on select API_COUNT_CALL for user ' . $userid, LOG_ERR);
 		$error++;
 	}
 
 	if ($error) {
 		$db->rollback();
-	} else {
+	}
+	else {
 		$db->commit();
 	}
 }
 
 // Call API termination method
-$apiMethodInfo = &$api->r->apiMethodInfo;
+$apiMethodInfo = & $api->r->apiMethodInfo;
 $terminateCall = '_terminate_' . $apiMethodInfo->methodName . '_' . $api->r->responseFormat->getExtension();
 if (method_exists($apiMethodInfo->className, $terminateCall)) {
 	// Now flush output buffers so that response data is sent to the client even if we still have action to do in a termination method.

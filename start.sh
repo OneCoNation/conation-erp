027e9a4746c993f0ae676fcdb31eab6c255eb0b9
the name`s rynn <andiargonnawork@gmail.com>
2026-02-15 15:39:29 +0700

    modify start.sh

--- start.sh
+++ start.sh
@@ -20,8 +20,6 @@ fi
 # 2. SECURITY FIXES
 # ==========================================
 echo "🔒 Applying security fixes..."
-
-# Install Lock & Permission
 touch /app/documents/install.lock
 chmod 0444 /app/htdocs/conf/conf.php
 
@@ -48,8 +46,7 @@ clear_env = no
 catch_workers_output = yes
 EOF
 
-# B. Buat MIME TYPES Manual (Solusi Error Anda)
-# Kita buat file ini agar Nginx tahu cara membaca CSS/JS/Gambar
+# B. Buat MIME TYPES (Agar Gambar/CSS muncul)
 cat > /app/mime.types <<EOF
 types {
     text/html                             html htm shtml;
@@ -60,83 +57,48 @@ types {
     application/javascript                js;
     application/atom+xml                  atom;
     application/rss+xml                   rss;
-    text/mathml                           mml;
     text/plain                            txt;
-    text/vnd.sun.j2me.app-descriptor      jad;
-    text/vnd.wap.wml                      wml;
-    text/x-component                      htc;
     image/png                             png;
-    image/tiff                            tif tiff;
-    image/vnd.wap.wbmp                    wbmp;
-    image/x-icon                          ico;
-    image/x-jng                           jng;
-    image/bmp                             bmp;
     image/svg+xml                         svg svgz;
     image/webp                            webp;
     application/font-woff                 woff;
-    application/java-archive              jar war ear;
     application/json                      json;
-    application/mac-binhex40              hqx;
-    application/msword                    doc;
     application/pdf                       pdf;
-    application/postscript                ps eps ai;
-    application/rtf                       rtf;
-    application/vnd.apple.mpegurl         m3u8;
-    application/vnd.ms-excel              xls;
-    application/vnd.ms-fontobject         eot;
-    application/vnd.ms-powerpoint         ppt;
-    application/vnd.wap.wmlc              wmlc;
-    application/vnd.google-earth.kml+xml  kml;
-    application/vnd.google-earth.kmz      kmz;
-    application/x-7z-compressed           7z;
-    application/x-cocoa                   cco;
-    application/x-java-archive-diff       jardiff;
-    application/x-java-jnlp-file          jnlp;
-    application/x-makeself                run;
-    application/x-perl                    pl pm;
-    application/x-pilot                   prc pdb;
-    application/x-rar-compressed          rar;
-    application/x-redhat-package-manager  rpm;
-    application/x-sea                     sea;
-    application/x-shockwave-flash         swf;
-    application/x-stuffit                 sit;
-    application/x-tcl                     tcl tk;
-    application/x-x509-ca-cert            der pem crt;
-    application/x-xpinstall               xpi;
-    application/xhtml+xml                 xhtml;
-    application/xspf+xml                  xspf;
     application/zip                       zip;
     application/octet-stream              bin exe dll;
-    application/octet-stream              deb;
-    application/octet-stream              dmg;
-    application/octet-stream              iso img;
-    application/octet-stream              msi msp msm;
-    audio/midi                            mid midi kar;
-    audio/mpeg                            mp3;
-    audio/ogg                             ogg;
-    audio/x-m4a                           m4a;
-    audio/x-realaudio                     ra;
-    video/3gpp                            3gpp 3gp;
-    video/mp2t                            ts;
-    video/mp4                             mp4;
-    video/mpeg                            mpeg mpg;
-    video/quicktime                       mov;
-    video/webm                            webm;
-    video/x-flv                           flv;
-    video/x-m4v                           m4v;
-    video/x-mng                           mng;
-    video/x-ms-asf                        asx asf;
-    video/x-ms-wmv                        wmv;
-    video/x-msvideo                       avi;
 }
 EOF
 
-# C. Buat Config NGINX (Arahkan ke MIME yg baru dibuat)
+# C. Buat FASTCGI PARAMS (Solusi Error Anda Sekarang!)
+# File ini wajib agar Nginx bisa ngobrol sama PHP
+cat > /app/fastcgi_params <<EOF
+fastcgi_param  QUERY_STRING       \$query_string;
+fastcgi_param  REQUEST_METHOD     \$request_method;
+fastcgi_param  CONTENT_TYPE       \$content_type;
+fastcgi_param  CONTENT_LENGTH     \$content_length;
+fastcgi_param  SCRIPT_NAME        \$fastcgi_script_name;
+fastcgi_param  REQUEST_URI        \$request_uri;
+fastcgi_param  DOCUMENT_URI       \$document_uri;
+fastcgi_param  DOCUMENT_ROOT      \$document_root;
+fastcgi_param  SERVER_PROTOCOL    \$server_protocol;
+fastcgi_param  REQUEST_SCHEME     \$scheme;
+fastcgi_param  HTTPS              \$https if_not_empty;
+fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+fastcgi_param  SERVER_SOFTWARE    nginx;
+fastcgi_param  REMOTE_ADDR        \$remote_addr;
+fastcgi_param  REMOTE_PORT        \$remote_port;
+fastcgi_param  SERVER_ADDR        \$server_addr;
+fastcgi_param  SERVER_PORT        \$server_port;
+fastcgi_param  SERVER_NAME        \$server_name;
+fastcgi_param  REDIRECT_STATUS    200;
+EOF
+
+# D. Buat Config NGINX Utama
 cat > /app/nginx.conf <<EOF
 worker_processes 1;
 events { worker_connections 1024; }
 http {
-    # Arahkan ke file mime.types yang baru kita buat di /app
+    # Include file yang baru kita buat di atas
     include /app/mime.types;
     
     default_type application/octet-stream;
@@ -152,7 +114,6 @@ http {
         root /app/htdocs;
         index index.php index.html;
 
-        # Blokir akses folder sensitif
         location ~ ^/(conf|includes|install|custom|documents)/ {
             deny all;
         }
@@ -162,10 +123,11 @@ http {
         }
 
         location ~ \.php$ {
-            include fastcgi_params;
+            # Gunakan file fastcgi_params yang baru kita buat
+            include /app/fastcgi_params;
+            
             fastcgi_pass 127.0.0.1:9000;
             fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-            fastcgi_param HTTPS 'on'; 
         }
     }
 }
